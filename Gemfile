15248bc7551eedbb4db434f53d1681c432071ec4
Alexander Simonov <alex@simonov.me>
2016-06-08 00:46:52 +0300

    Latest changes

--- Gemfile
+++ Gemfile
@@ -1,25 +1,26 @@
 source "https://rubygems.org"
 group :lint do
   gem 'foodcritic', '>= 4.0.0'
-  gem 'rubocop', '~> 0.18'
+  gem 'rubocop'
 end
 
 group :unit do
   gem 'berkshelf',  '>= 3.1'
+  gem 'solve', '2.0.2'
   gem 'chefspec',   '>= 4.0'
 end
 
 group :kitchen_common do
-  gem 'test-kitchen', github: 'test-kitchen/test-kitchen'
+  gem 'test-kitchen' #, github: 'test-kitchen/test-kitchen'
 end
 
 group :kitchen_vagrant do
-  gem 'kitchen-vagrant', '~> 0.11'
+  gem 'kitchen-vagrant'
 end
 
 group :development do
   gem 'rake'
-  gem 'rspec', '~> 3.0.0'
+  gem 'rspec'
   gem 'chef-zero'
   gem 'pry'
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,6 +16,11 @@ node.set['postgresql']['pg_hba'] = [
   { type: 'host',  db: 'all', user: 'all', addr: '::1/128', method: 'md5' },
   { type: 'host',  db: 'all', user: 'all', addr: '127.0.0.1/32', method: 'md5' }
 ]
+# node.set['postgresql']['dir'] = "/etc/postgresql/#{node['postgresql']['version']}/main"
+# node.set['postgresql']['client']['packages'] = [ "postgresql#{node['postgresql']['version'].sub('.', '')}", "postgresql#{node['postgresql']['version'].sub('.', '')}-devel" ]
+# node.set['postgresql']['server']['packages'] = [ "postgresql#{node['postgresql']['version'].sub('.', '')}-server" ]
+# node.set['postgresql']['server']['service_name'] = "postgresql#{node['postgresql']['version'].sub('.', '')}"
+# node.set['postgresql']['contrib']['packages'] = [ "postgresql-#{node['postgresql']['version'].sub('.', '')}-contrib" ]
 node.set['postgresql']['config_pgtune']['db_type'] = 'web'
 node.set['postgresql']['config']['port'] = '5432'
 node.set['postgresql']['config']['listen_addresses'] = 'localhost'
@@ -25,7 +30,7 @@ node.set['postgresql']['config']['log_rotation_age'] = '1d'
 node.set['postgresql']['enable_pgdg_yum'] = true if platform_family?('rhel')
 node.set['postgresql']['enable_pgdg_apt'] = true if platform_family?('debian')
 # workaround for reload attributes from postgresql cookbook after we set postgresql version
-node.from_file(run_context.resolve_attribute('postgresql', 'default'))
+# node.from_file(run_context.resolve_attribute('postgresql', 'default'))
 
 include_recipe 'postgresql::server'
 include_recipe 'postgresql::contrib'
